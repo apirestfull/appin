9af686ac2e387815d4783fd44745f22c1fa45a66
apirestfull <67928909+apirestfull@users.noreply.github.com>
2020-07-06 16:23:55 -0500

    Add files via upload

---
+++ EjemploBLL.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using ENTIDADES.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BLL
+{
+    public class EjemploBLL
+    {
+        public List<EjemploDTO> GetObtenerDatos()
+        {
+            EjemploDAL dato = new EjemploDAL();
+            return dato.obtenerDatos();
+        }
+        
+    }
+}
