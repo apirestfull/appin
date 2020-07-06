c3bb9f595324142848f132fbce731aed64341dcb
apirestfull <67928909+apirestfull@users.noreply.github.com>
2020-07-06 16:26:35 -0500

    Add files via upload

---
+++ EjemploDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENTIDADES.DTO
+{
+    public class EjemploDTO
+    {
+
+        public string nombre { get; set; }
+        public string tipo_doc { get; set; }       
+
+    }
+}
